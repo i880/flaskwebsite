8df75ce68a8f9513095a216e11a09e4ad6bf550f
i880 <ibrahimjr@4146c987a9a6>
2024-08-26 11:50:34 +0000

    change main file to flask_app for working on pythonanywhere

---
+++ flask_app.py
@@ -0,0 +1,12 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
